f1642d4b8e0279ad9e38bf37429070e6979b7923
cindy <cindy.nicolay@laposte.net>
2017-09-08 11:23:00 +0200

    header.php and images

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+
+      <div class="header">
+        <img src="image/logo.jpg" alt="logo">
+        <h1>E.Frozen</h1>
+        <span>Welcome to my website on products dysney Frozen</span>
+      </div>
+
+</header>
--- index.php
+++ index.php
@@ -18,13 +18,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico in the root directory -->
-        
+
         <link rel="stylesheet" href="css/materialize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
-        
+
+<!--**************HEADER******************************-->
+<?php include ("header.php");?>
+
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
