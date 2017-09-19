e60681261242d4f45d09d8315e334e90ef6d4307
SylvainB59 <s.baillot59@gmail.com>
2017-09-19 16:11:49 +0200

    Update formulaire.php
    fix conflict

--- formulaire.php
+++ formulaire.php
@@ -1,40 +1,10 @@
-<<<<<<< HEAD
-<!doctype html>
-<html class="no-js" lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>E-commerce PHP</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-        <!-- Place favicon.ico in the root directory -->
-
-        <link rel="stylesheet" href="css/materialize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
-
-
-        <div class="row">
+<div class="row">
     <form id="ajout" method="post" action="cible.php" class="col s12">
       <div class="row">
         <div class="input-field col s6">
           <input name="titre" type="text" class="validate">
           <label for="titre">Titre</label>
-=======
-      <form id="ajout">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        </div>
-        <div class="form-group">
-          <label for="titre">Titre du produit</label>
-          <input type="text" class="form-control" id="titre">
->>>>>>> c55507a65443c5ccbc56e3dd4d3def4d2b4ed510
+
         </div>
         <div class="input-field col s6">
           <input name="prix" type="text" class="validate">
